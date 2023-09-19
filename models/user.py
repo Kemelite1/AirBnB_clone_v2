05046f55c8431f5cca929f892d73eac1e409109c
Kemelite1 <osikemelite42@gmail.com>
2023-09-19 16:33:03 -0700

    updated base_model

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes
