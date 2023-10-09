ff94d1ded24a56c7201172bdff01b41d2b9a3183
Kemelite1 <osikemelite42@gmail.com>
2023-10-09 00:34:14 +0000

    distributes archive to the servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,24 +17,37 @@ def do_deploy(archive_path):
     Upload the archive to the /tmp/ directory of the web server
     Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
     Delete the archive from the web server"""
-    if not os.path.exists(archive_path):
-        print(archive_path)
-        return False
-    try:
-        fl = archive_path.split("/")[1]
-        r = fl.split(".")[0]
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(r))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(fl, r))
-        run(
-            "mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(
-                r, r
-            )
-        )
-        run("rm -rf /data/web_static/releases/{}/web_static".format(r))
-        run("rm -rf /tmp/{}".format(fl))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(r))
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
         return True
-    except Exception as e:
-        print(e)
+
+    return False
