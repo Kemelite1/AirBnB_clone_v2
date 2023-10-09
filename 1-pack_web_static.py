6486efa0fba6337aaa6a19e9ade9a1e30cb9111a
Kemelite1 <osikemelite42@gmail.com>
2023-10-09 00:25:02 +0000

    a Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,15 +13,26 @@ def do_pack():
 
     Prototype: def do_pack():
     All files in the folder web_static must be added to the final archive
-    All archives must be stored in the folder versions (your function should create this folder if it doesn’t exist)
+    All archives must be stored in the folder versions (your function should create this folder if it does not exist)
     The name of the archive created must be web_static_<year><month><day><hour><minute><second>.tgz
     The function do_pack must return the archive path if the archive has been correctly generated. Otherwise, it should return None
     """
-try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
-    except:
+    date_now = datetime.now()
+
+    if not os.path.exists("versions") and not os.path.isdir("versions"):
+        local("mkdir -p  versions")
+
+    filepth = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        date_now.year,
+        date_now.month,
+        date_now.day,
+        date_now.hour,
+        date_now.minute,
+        date_now.second,
+    )
+    local("tar -cvzf {} web_static".format(filepth))
+
+    if os.path.exists(filepth):
+        return filepth
+    else:
         return None
