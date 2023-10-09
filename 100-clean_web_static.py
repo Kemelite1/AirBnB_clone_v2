2f73f185e996a46baef5444f4b5a890bd9448f9a
Kemelite1 <osikemelite42@gmail.com>
2023-10-09 01:34:04 +0000

    deletes out-of-date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-
 from fabric.api import *
-
 
 env.hosts = ["52.201.69.229", "54.152.232.249"]
 
-
 def do_clean(number=0):
- """Delete out-of-date archives.
+    """deletes out-of-date archives"""
 
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
- number = int(number)
+    number = int(number)
 
     if number == 0:
         number = 2
     else:
         number += 1
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    local("cd versions ; ls -t | tail -n +{} | xargs rm -rf".format(number))
+    path = "/data/web_static/releases"
+    run("cd {} ; ls -t | tail -n +{} | xargs rm -rf".format(path, number))
